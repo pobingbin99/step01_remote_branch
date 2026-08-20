41e61a2fb786d7746c7d2e48ed511487fb8b140e
pobingbin99 <leedmswls@gmail.com>
2026-08-20 11:10:47 +0900

    git stash pop  충돌 해결

--- src/mvc/remote/StudentView.java
+++ src/mvc/remote/StudentView.java
@@ -1,7 +1,10 @@
 package mvc.remote;
 
-public class StudentView {
+public class StudentView { 
 	public void print() {
 		System.out.println("remote에서 수정");
+
+	public void print2() {
+		System.out.println("로컬에서 수정");
 	}
 }
